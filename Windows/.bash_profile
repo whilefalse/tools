170e5f2e50884bf5a20ca36736fcb5811715ac7f
Steven Anderson <steve@whilefalse.net>
2019-10-13 13:00:59 +0100

    Windoze

--- Windows/.bash_profile
+++
@@ -1,3 +0,0 @@
-if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
-
-[ -s "/c/Users/steven.anderson.NONLINEAR/.dnx/dnvm/dnvm.sh" ] && . "/c/Users/steven.anderson.NONLINEAR/.dnx/dnvm/dnvm.sh" # Load dnvm
--- Windows/setup_links.bat
+++
@@ -1,8 +0,0 @@
-mklink /H %UserProfile%\.bash_profile .bash_profile
-mklink /H %UserProfile%\.bashrc .bashrc
-mklink /H %UserProfile%\.gitconfig .gitconfig
-mklink /H %UserProfile%\.gitignore .gitignore
-mklink /H %UserProfile%\.inputrc .inputrc
-mklink /H %AppData%\Code\User\keybindings.json VSCode\keybindings.json
-mklink /H %AppData%\Code\User\settings.json VSCode\settings.json
-
