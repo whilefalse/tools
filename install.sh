64a741cba7418eabd206bf139f38d48690d6e301
Steven Anderson <steve@whilefalse.net>
2019-10-13 14:20:17 +0100

    Add homebrew and casks.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # Dotfiles: vim, tmux, zsh etc
+echo "=== Dot files ==="
 for x in `ls -A dotfiles`; do
   cmd="ln -sh"
   if [ "$1" == "-f" ]; then
@@ -10,6 +11,7 @@ for x in `ls -A dotfiles`; do
 done
 
 # VS Code
+echo "\n=== VS Code ==="
 for x in `ls vscode`; do
   cmd="ln -s"
   if [ "$1" == "-f" ]; then
@@ -19,8 +21,28 @@ for x in `ls vscode`; do
   echo $cmd
   eval $cmd
 done
+cmd="defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false"
+echo $cmd
+$cmd
+
+# Homebrew
+echo "\n=== HomeBrew ==="
+for x in `cat brew.txt`; do
+  cmd="brew install $x"
+  echo $cmd
+  $cmd
+done
+
+# Brew casks
+echo "\n=== HomeBrew Casks ==="
+for x in `cat brew.cask.txt`; do
+  cmd="brew cask install $x"
+  echo $cmd
+  $cmd
+done
 
 # Karabiner
+echo "\n=== Karabiner ==="
 cmd="ln -s"
 if [ "$1" == "-f" ]; then
   cmd+="f"
@@ -31,4 +53,7 @@ $cmd
 
 echo "\n=== MANUAL STEPS ==="
 # iTerm2
-echo "Now go to iTerm Prferences, check \"Load preferences from a custom folder or URL\" and enter `pwd`/config"
+echo "1. Go to iTerm Prferences, check \"Load preferences from a custom folder or URL\" and enter `pwd`/config"
+echo "2. Increase Key Repeat and decrease Delay Until Repeat in Sytem Preferences"
+echo "3. Install Oh My Zsh if it's not installed. Then restore ~/.zshrc"
+echo "4. Set Zsh as login shell if it's not already"
